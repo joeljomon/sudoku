78b6657a0b5d2a8ae77288f84557a43da8a95e9d
jjop04 <111251511+joeljomon@users.noreply.github.com>
2025-05-17 12:25:03 -0400

    Update puzzle.c

--- puzzle.c
+++ puzzle.c
@@ -1,5 +1,43 @@
 #include "sudoku.h"
 
+Square *** setUpPuzzle(int ** puzzle)
+{
+  Square *** sudoku;
+  int i, j;
+
+
+  sudoku = (Square***)malloc(sizeof(Square**)*9);
+
+  for (i = 0; i < SIZE_ROWS; i++) 
+  {
+    sudoku[i] = (Square**)malloc(sizeof(Square*)*9);
+
+    for (j = 0; j < SIZE_COLUMNS; j++)
+    {
+      sudoku[i][j] = (Square*)malloc(sizeof(Square)*9);
+      
+      sudoku[i][j] -> number = puzzle[i][j];
+
+      sudoku[i][j] -> row = i;
+      sudoku[i][j] -> column = j;
+      
+      if (sudoku[i][j] -> number != 0)
+      {
+        sudoku[i][j] -> code = POSSIBLE;
+      }
+      else 
+      {
+        sudoku[i][j] -> code = 0x0;
+      }
+
+    }
+  }
+
+
+}
+
+
+
 int ** createPuzzle()
 {
   
@@ -20,11 +58,11 @@ int ** createPuzzle()
   
   puzzle = (int**)malloc(sizeof(int*) *9);
   
-  for (i = 0; i < 9; i++) 
+  for (i = 0; i < SIZE_ROWS; i++) 
   {
     puzzle[i] = (int*)malloc(sizeof(int)*9);
 
-    for (j = 0; j < 9; j++)
+    for (j = 0; j < SIZE_COLUMNS; j++)
     {
       puzzle[i][j] = array[i][j];
 
@@ -38,12 +76,12 @@ void printPuzzle(int ** puzzle)
   int i, j;
 
   printf("-------------------------------\n");
-  for (i = 0; i < 9; i++)
+  for (i = 0; i < SIZE_ROWS; i++)
   {
 
     printf("|");
     /* print each row */
-    for (j = 0; j < 9; j++)
+    for (j = 0; j < SIZE_COLUMNS; j++)
     {
       printf(" %d ", puzzle[i][j]);
 
