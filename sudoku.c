a43e37f872520b25189d2173311168fd24e98b15
jjop04 <111251511+joeljomon@users.noreply.github.com>
2025-05-17 12:25:26 -0400

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -1,12 +1,19 @@
 #include "sudoku.h"
 
+int POSSIBLE = 511;
+int SIZE_ROWS = 9;
+int SIZE_COLUMNS = 9;
+
 int main()
 {
   int ** puzzle;
+  Square *** sudoku;
 
   puzzle = createPuzzle();
 
   printPuzzle(puzzle);
+
+  sudoku = setUpPuzzle(puzzle);
 
   return 0;
 }
